689225e563e14991ab47da47590da8476acafbb9
Jonas <44618301+jonazz1995@users.noreply.github.com>
2024-12-07 23:56:14 +0000

    futher changes

--- gallery/views.py
+++ gallery/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Image
 
-# Create your views here.
+# View to display the gallery page
+def gallery(request):
+    # Retrieve all images from the database
+    images = Image.objects.all()
+
+    # Pass the images to the template
+    return render(request, 'gallery/gallery.html', {'images': images})
--- imageWebsite/wsgi.py
+++ imageWebsite/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'imageWebsite.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "imageWebsite.settings")
 
 application = get_wsgi_application()
