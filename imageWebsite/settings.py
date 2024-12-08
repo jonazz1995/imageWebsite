aae12c91e36a19cc4639c4f63c8beb89d25c6248
Jonas <44618301+jonazz1995@users.noreply.github.com>
2024-12-08 00:12:07 +0000

    added code to serve media in dev

--- imageWebsite/settings.py
+++ imageWebsite/settings.py
@@ -123,8 +123,9 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
-MEDIA_URL = "/media/"
-MEDIA_ROOT = BASE_DIR / "media"
+# Path to media files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 # Default primary key field type
